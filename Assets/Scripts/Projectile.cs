7ffe09e0f72461e0ed1fda13ce6e4b488559dd77
neelesh-roxban <53631180+neelesh-roxban@users.noreply.github.com>
2021-03-15 16:39:29 +0530

    Initial commit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Projectile : MonoBehaviour
+{
+
+    
+    
+    Rigidbody2D rb;
+
+    public float dgrees;
+    float angle;
+    
+    public float force;
+    Vector2 direction;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();       
+       
+    }
+
+    private void Update()
+    {
+        getDirection();
+        move();
+
+    }
+
+    
+    void getDirection()
+    {
+        angle = dgrees * Mathf.Deg2Rad;
+        direction.x = Mathf.Cos(angle);
+        direction.y = Mathf.Sin(angle);
+
+    }
+    private void move()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          
+            rb.velocity =direction.normalized * force;
+        }
+    }
+    
+   
+}
