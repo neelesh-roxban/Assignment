1e1f1b5565509fa06c6c0a3fbd660e79416fe62b
neelesh-roxban <53631180+neelesh-roxban@users.noreply.github.com>
2021-03-16 19:39:41 +0530

    Movement In Circle

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public Transform player; 
+    public Vector3 circleCenter; 
+    public float radius;
+
+    void OnDrawGizmosSelected()
+    {
+        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+    void Update()
+    {
+       
+        float dist = Vector3.Distance(player.position, circleCenter); 
+
+        if (dist > radius)
+        {
+            Vector3 fromOrigintoObject = player.position - circleCenter;
+            fromOrigintoObject *= radius / dist;
+            player.position = circleCenter + fromOrigintoObject;
+            transform.position = player.position;
+        }
+    }
+}
+
